16038f4bfe3dc0a5b26e64286a217368bad866f4
Thiago Magano <thiagomagano1993@outlook.com>
2023-05-29 15:23:37 -0300

    Game fineshed with pwa

--- src/App.js
+++ src/App.js
@@ -1,24 +1,91 @@
-import React, { useState } from 'react';
+import { useState, useEffect } from 'react';
 import shuffle from './utilities/shuffle';
 import Card from './components/Card';
+import Header from './components/Header';
+import useAppBadge from './hooks/useAppBadge';
 
 function App() {
   const [cards, setCards] = useState(shuffle);
+  const [pickOne, setPickOne] = useState(null);
+  const [pickTwo, setPickTwo] = useState(null);
+  const [disabled, setDisabled] = useState(false);
+  const [wins, setWins] = useState(0);
+  const [setBadge, clearBadge] = useAppBadge();
+
+  const handleClick = card => {
+    if (!disabled) {
+      pickOne ? setPickTwo(card) : setPickOne(card)
+    }
+  };
+
+  const handleTurn = () => {
+    setPickOne(null);
+    setPickTwo(null);
+    setDisabled(false);
+  };
+
+  const handleNewGame = () => {
+    setWins(0);
+    clearBadge();
+    handleTurn();
+    setCards(shuffle);
+  };
+
+  useEffect(() => {
+    let pickTimer;
+
+    if (pickOne && pickTwo) {
+      if (pickOne.image === pickTwo.image) {
+        setCards((prevCards) => {
+          return prevCards.map((card) => {
+            if (card.image === pickOne.image) {
+              return { ...card, matched: true };
+            } else {
+              return card;
+            }
+          });
+        });
+        handleTurn();
+      } else {
+        setDisabled(true);
+
+        pickTimer = setTimeout(() => {
+          handleTurn();
+        }, 1000);
+      }
+    }
+
+    return () => {
+      clearTimeout(pickTimer);
+    }
+  }, [cards, pickOne, pickTwo, setBadge, wins])
 
 
+  useEffect(() => {
+
+    const checkWin = cards.filter((card) => !card.matched);
+
+    if (cards.length && checkWin.length < 1) {
+      console.log('You win!');
+      setWins(wins + 1);
+      setBadge();
+      handleTurn();
+      setCards(shuffle);
+    }
+  }, [cards, setBadge, wins]);
   return (
     <>
+      <Header handleNewGame={handleNewGame} wins={wins} />
       <div className="grid">
-        {
-          cards.map((card) => {
-            const { image, id, matched } = card;
-            return <Card
-              key={id}
-              image={image}
-              selected={false}
-              onClick={() => { }}
-            />
-          })
+        {cards.map((card) => {
+          const { image, matched } = card;
+          return <Card
+            key={image.id}
+            image={image}
+            selected={card === pickOne || card === pickTwo || matched}
+            onClick={() => handleClick(card)}
+          />
+        })
         }
       </div>
     </>
